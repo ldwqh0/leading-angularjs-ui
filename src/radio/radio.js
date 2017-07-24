f9b18d08919d850abf599d8e3ce37adb470b0112
xz <ldwqh0@outlook.com>
2017-07-24 13:21:43 +0800

    增加了属性required,disabled处理

--- src/radio/radio.js
+++ src/radio/radio.js
@@ -1,9 +1,10 @@
 import module from '../module'
 import './radio.less'
+
 let component = {
   template: `<label class="radio">
-               <span class="checker" ng-class="{'checked':$ctrl.ngModel==$ctrl.ngValue}">
-                 <input ng-required="$ctrl.required" ng-value="$ctrl.ngValue" ng-model="$ctrl.ngModel" name="{{$ctrl.name}}" type="radio">
+               <span class="checker" ng-class="{'checked': $ctrl.ngModel==$ctrl.ngValue}">
+                 <input ng-disabled="$ctrl.disabled" ng-required="$ctrl.required" ng-value="$ctrl.ngValue" value="$ctrl.value" ng-model="$ctrl.ngModel" name="{{$ctrl.name}}" type="radio">
                </span>
                <span>{{$ctrl.heading}}</span>
              </label>`,
@@ -11,19 +12,32 @@ let component = {
     heading: '@',
     name: '@',
     ngModel: '=',
-    ngValue: '@'
+    ngValue: '<?',
+    value: '@?'
   },
   controller: RadioController
 }
-RadioController.$inject = ['$element']
+RadioController.$inject = ['$scope', '$element']
 
-function RadioController ($element) {
+function RadioController ($scope, $element) {
   let $ctrl = this
+  $ctrl.disabled = false
+  $ctrl.ngValue = true
   $ctrl.$onInit = function () {
-    if ($element.attr('required')) {
-      $ctrl.required = true
-    }
   }
+
+  $scope.$watch(() => {
+    return $element.attr('required')
+  }, v => {
+    $ctrl.required = v === 'required'
+  })
+
+  $scope.$watch(() => {
+    return $element.attr('disabled')
+  }, v => {
+    $ctrl.disabled = v === 'disabled'
+  })
 }
+
 module.component('radio', component)
 export default 'radio'
